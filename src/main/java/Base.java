ea42437c65c07e885b04601495296196468a1d04
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-09-10 13:03:46 +0300

    Задание №2

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,5 +1,45 @@
+
+
+import java.util.Scanner;
+
+
+//Реализовать сложение дробных чисел, которые пользователь вводит в консоль.
+//        Использовать комментарии и JavaDoc при описании метода. Использовать форматирование при выводе
+//        результата в консоль. Полученный результат округлять до 4-х знаков после запятой.
+
+
 public class Base {
-    public static void main(String[] args) {
-        System.out.println("kljh");
+    public static void main(String[] args) throws Exception {
+        System.out.println("введите своё дробное число");
+
+        Scanner scanner = new Scanner(System.in);
+
+        double one = scanner.nextDouble();
+        //дробное число 1
+        /** @param one input one*/
+
+        System.out.println("введите второе число");
+        double two = scanner.nextDouble();
+        //дробное число 2
+        /**@param two input two*/
+
+        System.out.println("введите знак: сложение '+', вычитание '-'," +
+                "умножение '*', деление '/', другие символы не допускаются ");
+        /** @param znak input  math operation symbol*/
+
+
+
+        Scanner scanner1 = new Scanner(System.in);
+        String symbol = scanner1.nextLine();
+
+
+
+        scanner.close();
+        scanner1.close();
+
+
+    System.out.printf("Cумма чисел: %.4f", one + two);
+
     }
+
 }
