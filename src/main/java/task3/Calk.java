cf61631161fca0fddc3be69150c808bf2af6e892
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-09-13 14:25:42 +0300

    Задание №3 калькулятор доработка

--- src/main/java/task3/Calk.java
+++ src/main/java/task3/Calk.java
@@ -4,35 +4,47 @@ import java.util.Scanner;
 
 public class Calk {
     public static void calk()  {
-        System.out.println("введите своё дробное число");
-
-        Scanner scanner = new Scanner(System.in);
-
-        double one = scanner.nextDouble();
         //дробное число 1
         /** @param one input one*/
+        System.out.println("введите своё дробное число");
+        Scanner scanner = new Scanner(System.in);
+        double one = scanner.nextDouble();
 
-        System.out.println("введите второе число");
-        double two = scanner.nextDouble();
+
+        System.out.println("введите знак: сложение '+', вычитание '-'," + "умножение '*', деление '/', другие символы не допускаются ");
+        Scanner sum = new Scanner(System.in);
+        String symbol = sum.nextLine();
+
+
         //дробное число 2
         /**@param two input two*/
+        System.out.println("введите второе число");
+        double two = scanner.nextDouble();
 
-        System.out.println("введите знак: сложение '+', вычитание '-'," +
-                "умножение '*', деление '/', другие символы не допускаются ");
-        /** @param znak input  math operation symbol*/
-
-
-
-        Scanner scanner1 = new Scanner(System.in);
-        String symbol = scanner1.nextLine();
 
 
 
         scanner.close();
-        scanner1.close();
+        scanner.close();
+        vivod(one, two, symbol);
+    }
 
+    public static void vivod(double one, double two, String symbol) {
+        if (symbol.equals("+")) {
+            //  Cумма
+            System.out.printf("Cумма : %.4f", one + two);
+            // деление
+        } else if (symbol.equals( "/")) {
+            System.out.printf("деление: %.4f", one / two);
+            // вычетание
+        } else if (symbol.equals( "-")) {
+            System.out.printf("вычетание: %.4f", one - two);
+            // Умножение
+        } else if (symbol.equals( "*")) {
+            System.out.printf("Умножение: %.4f", one * two);
+        } else
 
-        System.out.printf("Cумма чисел: %.4f", one + two);
+            System.out.println("вы ввели не верный символ, введите только + - * /");
 
     }
 }
