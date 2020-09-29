d53d37c5d310005f6f31d8307b57c034c7eaad82
silver-Sanchez <55756108+silver-Sanchez@users.noreply.github.com>
2020-09-29 17:45:17 +0300

    Задание №7
    Добавить обработку исключений в задание с ООП калькуляторам (например, исключение может возникнуть при делении на ноль)

---
+++ src/main/java/task7/Calculator.java
@@ -0,0 +1,42 @@
+package task7;
+
+import java.util.Scanner;
+
+
+//Реализовать калькулятор в стиле ООП. Архитектуру приложения продумать самостоятельно.
+
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("введите своё дробное число");
+            double one = scanner.nextDouble();
+
+
+            System.out.println("введите знак: сложение '+', вычитание '-'," + "умножение '*', деление '/', другие символы не допускаются ");
+            char operaciya = scanner.next().charAt(0);
+            Operation operation = new Operation();
+
+
+            System.out.println("введите второе дробное число");
+            double two = scanner.nextDouble();
+            Numeric bNumber = new Numeric();
+            Numeric aNumber = new Numeric();
+
+            bNumber.setNumber(two);
+            aNumber.setNumber(one);
+
+
+            operation.setOper(operaciya);
+            operation.setResult(one, two, operaciya);
+
+        }
+ catch (RuntimeException e)
+        {
+            System.out.println("ошибка работы. " + e.getLocalizedMessage());
+        }
+
+}
+
+}
---
+++ src/main/java/task7/Numeric.java
@@ -0,0 +1,16 @@
+package task7;
+
+public class Numeric {
+
+    double number;
+
+    public Numeric() {
+
+    }
+
+    public void setNumber(double number)
+    {
+        this.number = number;
+
+    }
+}
